ef48e3be840d8d3d5839367ea3b19e572541ab47
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2023-03-10 21:39:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function preload(){
 
 }
@@ -5,12 +6,27 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+   video.hide();
+
+   poseNet = ml5.poseNet(video, modelLoaded);
+   poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-
+    image(video,0,0,300,300);
 }
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');}
 
 function take_snapshot(){
 
 }
+
+function gotPoses(results)
+{
+    console.log(results);
+}
+
